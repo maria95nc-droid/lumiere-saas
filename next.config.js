21c4de9c2087f7e2404dac469ce9ab93c5251cc8
maria95nc-droid <maria.95nc@gmail.com>
2026-03-06 22:27:27 +0100

    fix root env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // Needed for prisma.config.ts support in Prisma 5.14+
-  },
-};
-
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */ 
